10c803760b2ad4a22a7cf8bd39d277f98dd5718d
Muhaymin21 <aaea55@gmail.com>
2021-08-18 16:40:02 +0300

    Restructure

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 from auth import AuthError, requires_auth
 
-app = Flask(__name__, static_folder='client/build/', static_url_path='/')
+app = Flask(__name__, static_folder='build/', static_url_path='/')
 
 
 @app.route('/api/')
--- heroku/run.sh
+++
@@ -1,1 +0,0 @@
-npm build --prefix client

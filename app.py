6599fbe01dcab3f9044850485cac675593f508af
Muhaymin21 <aaea55@gmail.com>
2021-08-20 16:44:30 +0300

    Add create movie

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from flask import Flask, jsonify
+import sys
+
+from flask import Flask, jsonify, request, abort
 from auth import AuthError, requires_auth
 from flask_migrate import Migrate
 from db_models import *
-
+from sqlalchemy import exc
 
 app = Flask(__name__, static_folder='build/', static_url_path='/')
 app.config.from_object('config')
 db.init_app(app)
 migrate = Migrate(app, db)
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/api/movies')
-def hello_world():
+# @requires_auth("write:movie")
+def get_movies():
     movies = [movie.format_output() for movie in Movie.query.order_by(Movie.id).all()]
     return jsonify({
         "success": True,
@@ -19,19 +28,33 @@ def hello_world():
     })
 
 
-@app.route('/api/protected/rate')
-@requires_auth("rate:movies")
-def hello_world_protected_with_permission(payload):
-    return jsonify(payload)
-
-
-@app.route('/api/protected')
-@requires_auth()
-def hello_world_protected():
-    return jsonify({
-        "success": True,
-        "message": "Hello, Protected!"
-    })
+@app.route('/api/movies/create', methods=['POST'])
+# @requires_auth("write:movie")
+def create_movie():
+    name = request.form.get('name')
+    description = request.form.get('description')
+    date = request.form.get('date')
+    img_path = request.form.get('imgPath')
+    if not name or not description or not date or not img_path:
+        abort(400)
+    try:
+        new_movie = Movie(
+            name=name,
+            description=description,
+            date=date,
+            rate=0,
+            img_path=img_path,
+        )
+        new_movie.insert()
+    except exc.SQLAlchemyError:
+        db.session.rollback()
+        print(sys.exc_info())
+        abort(500)
+    finally:
+        db.session.close()
+        return jsonify({
+            "success": True
+        })
 
 
 @app.route('/')
@@ -55,13 +78,23 @@ def not_found(error):
 
 
 @app.errorhandler(500)
-def not_found(error):
+def server_error(error):
     print(error)
     return jsonify({
         "success": False,
         "error": 500,
         "message": "The server failed to process the request"
     }), 500
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    print(error)
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "Bad request"
+    }), 400
 
 
 @app.errorhandler(AuthError)

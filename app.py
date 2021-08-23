c8843c870aa0c6e635059187ad4979c4978731fd
Muhaymin21 <aaea55@gmail.com>
2021-08-23 11:24:12 +0300

    Add get selected movie endpoint

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import sys
 
 from flask import Flask, jsonify, request, abort
 from flask_migrate import Migrate
+from sqlalchemy import exc, and_
 from werkzeug.exceptions import NotFound
 from auth import AuthError, requires_auth
 from db_models import *
@@ -12,30 +13,30 @@ db.init_app(app)
 migrate = Migrate(app, db)
 
 
+def get_rating_data(body):
+    if body is None:
+        abort(400, description="Please provide the data as json.")
+    if "newRate" not in body:
+        abort(400, description="Please insert the new rate.")
+    return body.get('newRate')
+
+
 def check_all_data_posted(body):
     if body is None:
         abort(400, description="Please provide the data as json.")
-    if "data" not in body:
-        abort(400, description="The data should be inside data key.")
-    body = body.get('data')
-    name = body.get('name')
-    description = body.get('description')
-    date = body.get('date')
-    img_path = body.get('image')
-
-    if name is None:
+    if 'name' not in body:
         abort(400, description="Please insert the movie name.")
-    if description is None:
+    if 'description' not in body:
         abort(400, description="Please insert the movie description.")
-    if date is None:
+    if 'date' not in body:
         abort(400, description="Please insert the movie date.")
-    if img_path is None:
+    if 'image' not in body:
         abort(400, description="Please insert the movie image url.")
     return {
-        "name": name,
-        "description": description,
-        "date": date,
-        "img_path": img_path
+        "name": body.get('name'),
+        "description": body.get('description'),
+        "date": body.get('date'),
+        "img_path": body.get('image')
     }
 
 
@@ -44,6 +45,14 @@ def paginate(page, selection, movies_per_page):
     end = start + movies_per_page
     movies = [movie.format_output() for movie in selection]
     return movies[start:end]
+
+
+def get_user_id(payload):
+    user_id = payload['sub'].split('|')[1]
+    if user_id is not None:
+        return user_id
+    else:
+        abort(401, "The server failed to retrieve your ID, please try to sign out then sign in again.")
 
 
 @app.route('/api/movies')  # get all movies
@@ -60,6 +69,8 @@ def get_movies():
     except:
         print(sys.exc_info())
         abort(500, description="The server failed to load the movies list, please try again later.")
+    finally:
+        db.session.close()
 
 
 # noinspection PyUnusedLocal
@@ -85,6 +96,27 @@ def create_movie(payload):
         db.session.rollback()
         print(sys.exc_info())
         abort(500, description="The server failed to insert the new movie, please try again later.")
+    finally:
+        db.session.close()
+
+
+@app.route('/api/movies/<int:movie_id>')  # GET selected movie
+def get_movie(movie_id):
+    try:
+        movie = Movie.query.filter_by(id=movie_id).one_or_none()
+        if movie is not None:
+            return jsonify({
+                "success": True,
+                "movie": movie.format_output()
+            })
+        else:
+            raise NotFound()
+    except NotFound:
+        raise ResourceNotFound("This movie does not exist.")
+    except:
+        db.session.rollback()
+        print(sys.exc_info())
+        abort(500, description="The server failed to get the movie, please try again later.")
     finally:
         db.session.close()
 
@@ -144,44 +176,97 @@ def edit_movie(payload, movie_id):
 @app.route('/api/users/rates')
 @requires_auth("")  # User id is fetched from the token payload
 def get_user_rates(payload):
-    user_id = payload['sub'].split('|')[1]
+    user_id = get_user_id(payload)
     # example payload {"sub": "auth0|611fbf35b540dd00l3fca567"}
-    if user_id is not None:
-        body = request.get_json()
-        if body is not None:
-            rates_id_list = body.get("rates")  # type: list
-            if rates_id_list is not None:
-                try:
-                    rates = [
-                        rate.format_output() for rate in Rate.query.filter_by(user_id=user_id).all()
-                        if rate.movie_id in rates_id_list
-                    ]
-                    return jsonify({"rates": rates})
-                except:
-                    print(sys.exc_info())
-                    abort(500, description="The server failed to get selected movies rate, please try again later.")
-            else:
-                abort(400, "Please include the selected movies id list")
+    body = request.get_json()
+    if body is not None:
+        rates_id_list = body.get("rates")  # type: list
+        if rates_id_list is not None:
+            try:
+                rates = [
+                    rate.format_output() for rate in Rate.query.filter_by(user_id=user_id).all()
+                    if rate.movie_id in rates_id_list
+                ]
+                return jsonify({
+                    "success": True,
+                    "rates": rates
+                })
+            except:
+                print(sys.exc_info())
+                abort(500, description="The server failed to get selected movies rate, please try again later.")
         else:
-            abort(400, "Please post json contain selected movies id list")
+            abort(400, "Please include the selected movies id list")
     else:
-        abort(401, "The server failed to retrieve your ID, please try to sign out then sign in again.")
+        abort(400, "Please post json contain selected movies id list")
 
 
 @app.route('/api/movies/<int:movie_id>/rate', methods=['POST'])  # Rating movie the first time
 @requires_auth("write:rate")
 def rate_movie(payload, movie_id):
-    return jsonify({
-        "movieID": movie_id
-    })
+    user_id = get_user_id(payload)
+    rate = get_rating_data(request.get_json())
+    try:
+        movie = Movie.query.filter_by(id=movie_id).one_or_none()
+        if movie is None:
+            raise NotFound()
+        new_rate = movie.all_rates_total + rate
+        movie.all_rates_total = new_rate
+        movie.all_rates_count = movie.all_rates_count + 1
+        Rate(
+            movie_id=movie_id,
+            user_id=user_id,
+            rate=rate
+        ).insert()
+        return jsonify({
+            "success": True,
+            "movieID": movie_id,
+            "rate": rate
+        })
+    except NotFound:
+        raise ResourceNotFound("This movie does not exist.")
+    except exc.IntegrityError as error:
+        print(error)
+        db.session.rollback()
+        abort(500, description="This movie is already rated.")
+    except:
+        print(sys.exc_info())
+        db.session.rollback()
+        abort(500, description="The server failed to insert the selected movies rate, please try again later.")
+    finally:
+        db.session.close()
 
 
 @app.route('/api/movies/<int:movie_id>/rate', methods=['PATCH'])  # Edit rated movie
 @requires_auth("write:rate")
 def update_rated_movie(payload, movie_id):
-    return jsonify({
-        "movieID": movie_id
-    })
+    user_id = get_user_id(payload)
+    rate = get_rating_data(request.get_json())
+    try:
+        old_rate = Rate.query.filter(and_(
+            Rate.movie_id == movie_id, Rate.user_id == user_id
+        )).one_or_none()  # type: Rate
+        if old_rate is None:
+            raise NotFound()
+        movie = Movie.query.filter_by(id=movie_id).one_or_none()
+        if movie is None:
+            raise NotFound()
+        new_rate = movie.all_rates_total + (rate - old_rate.rate)
+        old_rate.rate = rate
+        movie.all_rates_total = new_rate
+        db.session.commit()
+        return jsonify({
+            "success": True,
+            "movieID": movie_id,
+            "newRate": rate
+        })
+    except NotFound:
+        raise ResourceNotFound("This movie does not exist or the data have integrity error.")
+    except:
+        print(sys.exc_info())
+        db.session.rollback()
+        abort(500, description="The server failed to insert the selected movies rate, please try again later.")
+    finally:
+        db.session.close()
 
 
 # -----------------  start - return react frontend ----------------- #

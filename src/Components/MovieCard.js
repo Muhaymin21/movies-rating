544cebd2876af64be7e70d23413605d9bf10d39c
Muhaymin21 <aaea55@gmail.com>
2021-08-25 00:17:01 +0300

    Snackbar callback function

--- src/Components/MovieCard.js
+++ src/Components/MovieCard.js
@@ -5,6 +5,7 @@ import {MoreVert, Share} from "@material-ui/icons";
 import {useAuth0} from "@auth0/auth0-react";
 import ReactStars from "react-rating-stars-component";
 import useShare from "../Hooks/useShare";
+import useSnackBar from "../Hooks/useSnackBar";
 import {Link} from "react-router-dom";
 import {
     Avatar,
@@ -13,7 +14,7 @@ import {
     CardContent,
     CardHeader,
     CardMedia,
-    IconButton, Menu, MenuItem,
+    IconButton, Menu, MenuItem, MenuList,
     Typography
 } from "@material-ui/core";
 import axios from "axios";
@@ -57,155 +58,183 @@ const useStyles = makeStyles((theme) => ({
 
 
 export default function MovieCard(props) {
-    const classes = useStyles();
-    const [rate, setRate] = React.useState(props.rate);
-    const {isAuthenticated} = useAuth0();
-    const url = window.location.origin + '/movies/' + props.id;
-    const [ShareModal, SnackBar, setShareModalOpen] = useShare(url);
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const [CustomDialog, selectedValue, setDialogOpen] = useDialog();
-    const scopes = useSelector((state) => state.scope.scopes);
+    const [Deleted, setDeleted] = React.useState(false);
+    const [SnackBar, setSnackBarMessage, setSnackBarOpen, setSnackBarType] = useSnackBar();
 
-    React.useEffect(() => {
-        if (selectedValue) {
-            axios.delete('/api/movies/' + props.id).then((r) => {
-                console.log(r);
-            }, (e) => {
-                console.log(e)
-            });
+    function MoiveCardChild() {
+        const classes = useStyles();
+        const [rate, setRate] = React.useState(props.rate);
+        const {isAuthenticated} = useAuth0();
+        const url = window.location.origin + '/movies/' + props.id;
+        const [ShareModal, setShareModalOpen] = useShare(url);
+        const [anchorEl, setAnchorEl] = React.useState(null);
+        const [CustomDialog, setDialogOpen] = useDialog();
+        const scopes = useSelector((state) => state.scope.scopes);
+
+        function alert(type, message) {
+             setSnackBarMessage(message);
+            setSnackBarType(type ? "success" : "error");
+            setSnackBarOpen(true);
         }
-    }, [selectedValue, props.id])
 
-    const handleMoreClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
+        const shareCallback = (message) => {
+            alert(true, message);
+        }
 
-    const handleMenuClose = () => {
-        setAnchorEl(null);
-    };
-
-    const ratingChanged = (newRating) => {
-        axios.patch(`/api/movies/${props.id}/rate`, {
-            newRate: newRating
-        }).then(
-            r => {
-                if (r.data.success)
-                    setRate(r.data['newRate']);
-            },
-            error => {
-                console.error(error);
+        const closeDialogCallback = (desicion) => {
+                        if (desicion) {
+                axios.delete('/api/movies/' + props.id).then((r) => {
+                    if (r.data.success)
+                    {
+                        setDeleted(true);
+                        alert(true, "Movie deleted successfully.");
+                    }
+                    else
+                        alert(false, "Failed to delete the movie.");
+                }, () => {
+                    alert(false, "Failed to delete the movie.");
+                });
             }
-        );
-    };
-
-    function ShortDesc() {
-        const len = props.description.length;
-        const [showMore, setShowMore] = React.useState(false);
-
-        function toggleReadMore() {
-            setShowMore(!showMore);
         }
 
-        if (len > 250)
-            return (
-                <Typography variant="body2" color="textPrimary" component="p">
-                    {showMore ? (
-                        <>{props.description} <span onClick={toggleReadMore}
-                                                    className={classes.more}>show less...</span></>
-                    ) : (
-                        <>{props.description.substring(0, 249)} <span onClick={toggleReadMore} className={classes.more}>show more...</span></>
-                    )}
-                </Typography>
-            )
-        else
-            return (<Typography variant="body2" color="textPrimary" component="p">{props.description}</Typography>);
-    }
+        const handleMoreClick = (event) => {
+            setAnchorEl(event.currentTarget);
+        };
 
-    function handleMovieDelete() {
-        setDialogOpen(true);
-        handleMenuClose();
-    }
+        const handleMenuClose = () => {
+            setAnchorEl(null);
+        };
 
-    return (
-        <>
-            <Card className={classes.root}>
-                <CardHeader
-                    avatar={
-                        <Avatar aria-label="recipe" className={classes.avatar}>
-                            {rate}
-                        </Avatar>
-                    }
-                    action={
+        const ratingChanged = (newRating) => {
+            axios.patch(`/api/movies/${props.id}/rate`, {
+                newRate: newRating
+            }).then(
+                r => {
+                    if (r.data.success)
+                        setRate(r.data['newRate']);
+                },
+                error => {
+                    console.error(error);
+                }
+            );
+        };
+
+        function ShortDesc() {
+            const len = props.description.length;
+            const [showMore, setShowMore] = React.useState(false);
+
+            function toggleReadMore() {
+                setShowMore(!showMore);
+            }
+
+            if (len > 250)
+                return (
+                    <Typography variant="body2" color="textPrimary" component="p">
+                        {showMore ? (
+                            <>{props.description} <span onClick={toggleReadMore}
+                                                        className={classes.more}>show less...</span></>
+                        ) : (
+                            <>{props.description.substring(0, 249)} <span onClick={toggleReadMore}
+                                                                          className={classes.more}>show more...</span></>
+                        )}
+                    </Typography>
+                )
+            else
+                return (<Typography variant="body2" color="textPrimary" component="p">{props.description}</Typography>);
+        }
+
+        function handleMovieDelete() {
+            setDialogOpen(true);
+            handleMenuClose();
+        }
+
+        return (
+            <>
+                <Card className={classes.root}>
+                    <CardHeader
+                        avatar={
+                            <Avatar aria-label="recipe" className={classes.avatar}>
+                                {rate}
+                            </Avatar>
+                        }
+                        action={
+                            <IconButton
+                                onClick={handleMoreClick}
+                                aria-label="settings">
+                                <MoreVert/>
+                            </IconButton>
+                        }
+                        title={props.title}
+                        subheader={props.date}
+                    />
+                    <CardMedia
+                        className={classes.media}
+                        image={props.image}
+                        title="poster"
+                    />
+                    <CardContent className={classes.textArea}>
+                        <ShortDesc/>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        {isAuthenticated && (
+                            <ReactStars
+                                onChange={ratingChanged}
+                                size={24}
+                                isHalf={true}
+                                value={props.stars}
+                            />
+                        )}
                         <IconButton
-                            onClick={handleMoreClick}
-                            aria-label="settings">
-                            <MoreVert/>
+                            onClick={async () => {
+                                if (typeof navigator.share === "function")
+                                    await navigator.share({
+                                        title: props.title,
+                                        text: props.description,
+                                        url
+                                    });
+                                else setShareModalOpen(true);
+                            }}
+                            aria-label="share" style={{marginLeft: 'auto',}}>
+                            <Share/>
                         </IconButton>
-                    }
-                    title={props.title}
-                    subheader={props.date}
-                />
-                <CardMedia
-                    className={classes.media}
-                    image={props.image}
-                    title="poster"
-                />
-                <CardContent className={classes.textArea}>
-                    <ShortDesc/>
-                </CardContent>
-                <CardActions disableSpacing>
-                    {isAuthenticated && (
-                        <ReactStars
-                            onChange={ratingChanged}
-                            size={24}
-                            isHalf={true}
-                            value={props.stars}
-                        />
-                    )}
-                    <IconButton
-                        onClick={async () => {
-                            if (typeof navigator.share === "function")
-                                await navigator.share({
-                                    title: props.title,
-                                    text: props.description,
-                                    url
-                                });
-                            else setShareModalOpen(true);
-                        }}
-                        aria-label="share" style={{marginLeft: 'auto',}}>
-                        <Share/>
-                    </IconButton>
-                </CardActions>
-            </Card>
-            <ShareModal/>
-            <SnackBar/>
-            <Menu
-                id="menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleMenuClose}
-            >
-                <Link className={classes.linkStyle} to={"/movies/" + props.id}>
-                    <MenuItem onClick={handleMenuClose}>Open</MenuItem>
-                </Link>
+                    </CardActions>
+                </Card>
+                <ShareModal onClose={shareCallback} />
+                <SnackBar/>
+                <Menu
+                    id="menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleMenuClose}
+                >
+                        <Link className={classes.linkStyle} to={"/movies/" + props.id}>
+                            <MenuItem onClick={handleMenuClose}>Open</MenuItem>
+                        </Link>
+                        {isAuthenticated && (
+                            scopes.includes("delete:movie") && (
+                                <MenuList>
+                                    <MenuItem onClick={handleMenuClose}>Edit</MenuItem>
+                                    <MenuItem onClick={handleMovieDelete}>Delete</MenuItem>
+                                </MenuList>
+                            )
+                        )}
+                </Menu>
                 {isAuthenticated && (
                     scopes.includes("delete:movie") && (
-                        <>
-                            <MenuItem onClick={handleMenuClose}>Edit</MenuItem>
-                            <MenuItem onClick={handleMovieDelete}>Delete</MenuItem>
-                        </>
+                        <CustomDialog
+                            callback={closeDialogCallback}
+                            header="Are you sure?"
+                            body="Continue with this action will result in deleting this movie with all its data."
+                        />
                     )
                 )}
-            </Menu>
-            {isAuthenticated && (
-                scopes.includes("delete:movie") && (
-                    <CustomDialog
-                        header="Are you sure?"
-                        body="Continue with this action will result in deleting this movie with all its data."
-                    />
-                )
-            )}
-        </>
-    );
+            </>
+        );
+    }
+
+    if (Deleted)
+        return <SnackBar/>;
+    else
+        return <MoiveCardChild/>;
 }
